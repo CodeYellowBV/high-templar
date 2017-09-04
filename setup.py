0d54983be886b6a88f18768b61049a669fd6acb6
Jasper Stam <jasper@codeyellow.nl>
2017-09-04 11:53:37 +0200

    Update setup.py for pypi

--- setup.py
+++ setup.py
@@ -3,24 +3,33 @@
 import os
 from setuptools import find_packages, setup
 
-# with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-#     README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
 setup(
     name='high_templar',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
-    include_package_data=True,
     license='MIT',
-    description='A server which handles websockets for an existing django-binder instance',
+    description='A python framework for creating a server which handles websockets for an existing API',
+    long_description=README,
     author='Jasper Stam',
     author_email='jasper@codeyellow.nl',
     url='https://github.com/CodeYellowBV/high-templar',
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     install_requires=[
         'gevent >= 1.1.2',
         'greenlet >= 0.4.12',
@@ -30,5 +39,5 @@ setup(
         'python-dotenv >= 0.6.3',
         'requests >= 2.13.0',
     ],
-    test_suite='tests',
+    test_suite='tests'
 )
