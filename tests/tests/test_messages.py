53b398be1e104991a1eca8ea62cfbea1ad260101
stefanmajoor <stefan@codeyellow.nl>
2020-02-19 16:59:19 +0100

    add tests for which messages are relayed to the user

--- tests/tests/test_messages.py
+++ tests/tests/test_messages.py
@@ -120,3 +120,90 @@ class TestMessages(TestCase):
                 self.assertEquals(data, res)
 
         asyncio.get_event_loop().run_until_complete(run())
+
+    def test_subscribe_all_gets_message(self):
+        set_bootstrap_response({
+            "allowed_rooms": [{
+                "target": "message",
+                "customer": "*"
+            }]
+        })
+
+        async def run():
+            async with websockets.connect(WS_URI) as ws:
+                await ws.recv()
+
+                await ws.send(json.dumps({
+                    "type": "subscribe",
+                    "room": {
+                        "target": "message",
+                        "customer": "*"
+                    }
+                }))
+                await ws.recv()
+
+                data = {
+                    "foo": "bar"
+                }
+
+                send_trigger({
+                    "rooms": [
+                        {
+                            "target": "message",
+                            "customer": "1"
+                        }
+
+                    ],
+                    "data": data
+                })
+
+                res = await ws.recv()
+                res = json.loads(res)
+                del res['requestId']
+
+                self.assertEquals(data, res)
+
+        asyncio.get_event_loop().run_until_complete(run())
+
+    def test_subscribe_no_permission_no_message(self):
+        set_bootstrap_response({
+            "allowed_rooms": [{
+                "target": "message",
+                "customer": "2"
+            }]
+        })
+
+        async def run():
+            async with websockets.connect(WS_URI) as ws:
+                await ws.recv()
+
+                await ws.send(json.dumps({
+                    "type": "subscribe",
+                    "room": {
+                        "target": "message",
+                        "customer": "2"
+                    }
+                }))
+                await ws.recv()
+
+                data = {
+                    "foo": "bar"
+                }
+
+                send_trigger({
+                    "rooms": [
+                        {
+                            "target": "message",
+                            "customer": "1"
+                        }
+
+                    ],
+                    "data": data
+                })
+
+                # check that we are not notified after two seconds
+                with self.assertRaises(asyncio.TimeoutError):
+                    await asyncio.wait_for(ws.recv(), timeout=2)
+
+
+        asyncio.get_event_loop().run_until_complete(run())
