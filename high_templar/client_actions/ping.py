c21053953745d3160bf264b3b220a86fc3accc72
stefanmajoor <stefan@codeyellow.nl>
2020-02-20 11:12:54 +0100

    Add hooks

--- high_templar/client_actions/ping.py
+++ high_templar/client_actions/ping.py
@@ -1,2 +1,8 @@
+import asyncio
+
+
 async def ping(connection, message):
-    return await connection.send_raw('pong')
+    return await asyncio.gather(
+        connection.send_raw('pong'),
+        connection.app.notify_ping(connection)
+    )
