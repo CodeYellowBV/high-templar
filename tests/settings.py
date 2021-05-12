b0863c162263aba50f4799c47e13903684055a36
AB Zainuddin <burhan@codeyellow.nl>
2021-05-12 17:25:23 +0200

    Initial working version

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,8 @@
-WS_URI = 'ws://localhost:5000/ws/'
+WS_URI = 'ws://backend:5000/ws/'
 
 RABBITMQ = {
     'exchange_name': 'hightemplar',
     'username': 'rabbitmq',
     'password': 'rabbitmq',
-    'host': '127.0.0.1'
+    'host': 'rabbitmq'
 }
