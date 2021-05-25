ae1def933ba8ab9fe37dd24e18db545865a3e339
AB Zainuddin <burhan@codeyellow.nl>
2021-05-25 09:04:58 +0200

    Catch exception when unsubscribing twice

--- high_templar/client_actions/unsubscribe.py
+++ high_templar/client_actions/unsubscribe.py
@@ -1,7 +1,10 @@
+
 async def unsubscribe(connection, message):
     """
     Unsubscribe by finding the subscription based on message.requestId.
     """
+    from high_templar.hub import NotSubscribedException
+
     if 'requestId' not in message:
         return await connection.send({
             'code': 'error',
@@ -10,10 +13,14 @@ async def unsubscribe(connection, message):
 
     for subscription in connection.app.hub.subscriptions[connection]:
         if subscription.request_id == message['requestId']:
-            connection.app.hub.unsubscribe(subscription)
-            await connection.send({
-                'code': 'success',
-            })
+            try:
+                connection.app.hub.unsubscribe(subscription)
+                await connection.send({ 'code': 'success' })
+            except NotSubscribedException:
+                await connection.send({
+                    'code': 'error',
+                    'message': 'not-subscribed'
+                })
             break
     else:
         return await connection.send({
