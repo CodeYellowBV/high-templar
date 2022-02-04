40a60a9e201ed50b99c67004f8016feea385f9d9
knokko <knokogator@hotmail.com>
2022-02-04 16:40:43 +0100

    Test that BinderAdapter propagates app.config.SESSION_KWARGS to the constructor of ClientSession

--- tests/tests/test_adapter.py
+++ tests/tests/test_adapter.py
@@ -31,3 +31,19 @@ class TestAdapter(TestCase):
                 await adapter._request('GET', '')
             mock_method.assert_called_once_with('GET', '', headers={}, test1234='test1234')
         asyncio.get_event_loop().run_until_complete(run())
+
+    def test_session_kwargs_config(self):
+        """
+        Test that BinderAdapter propagates the SESSION_KWARGS in 
+        app.config to the ClientSession constructor
+        """
+
+        with patch.object(ClientSession, '__init__', return_value=None) as mock_method:
+            class Settings():
+                API_URL = ''
+                SESSION_KWARGS = {'test123': 123}
+            
+            app = create_app(Settings())
+            BinderAdapter(app)
+
+        mock_method.assert_called_once_with(test123=123)
